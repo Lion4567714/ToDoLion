0e1a9edcfbc37ccd5ea1af41d2b4d3a5d90654b6
Lion4567714 <ablion7714@icloud.com>
2024-04-13 01:56:03 -0400

    Added scaffold and padding

--- app/src/main/java/com/example/todolion/MainActivity.kt
+++ app/src/main/java/com/example/todolion/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,32 +13,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todolion.ui.theme.ToDoLionTheme
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ToDoLionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("World!")
-                }
+                Scaffold(
+                    modifier = Modifier.padding(24.dp)
+                ) { padding -> Home(Modifier.padding(padding)) }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Home(modifier: Modifier = Modifier) {
+    Column {
+        Greeting("Trash", modifier)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ToDoLionTheme {
-        Greeting("Android")
-    }
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Fuck you, $name!",
+        modifier = modifier,
+        color = Color(0xFFFFFFFF)
+    )
 }
